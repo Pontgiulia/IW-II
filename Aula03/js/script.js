8c5a7238fc6af4348aed007acd4924822f74a478
Pontgiulia <pontelligiulia@gmail.com>
2024-02-29 08:46:45 -0300

    aula dia 29/02

---
+++ Aula03/js/script.js
@@ -0,0 +1,21 @@
+// var result = document.getElementById('result')
+
+
+// function peganm(){
+//     var nome = document.getElementById('nome').value
+//     console.log('nome: ', nome)
+//     result.innerHTML = nome
+// }
+
+
+var subtr = (prim-segn)
+var mult = (prim*segn)
+var divi = (prim/segn)
+
+function fazsoma(){
+    var soma = (prim+segn)
+    console.log('soma ', soma)
+    result.innerHTML = soma
+}
+
+
