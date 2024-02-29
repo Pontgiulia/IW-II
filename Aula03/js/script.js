b27e83711f5c335d492646330cb2ebc418fd8c3a
Pontgiulia <pontelligiulia@gmail.com>
2024-02-29 09:09:14 -0300

    calculos

--- Aula03/js/script.js
+++ Aula03/js/script.js
@@ -8,14 +8,73 @@
 // }
 
 
-var subtr = (prim-segn)
-var mult = (prim*segn)
-var divi = (prim/segn)
+
+const somar = document.getElementById('soma')
+const subtracao = document.getElementById('subtr')
+const multiplicacao = document.getElementById('mult')
+const divisao = document.getElementById('divi')
+const limpar = document.getElementById('lim')
+
+const result = document.getElementById('result')
+
+const numer = document.getElementById('prim')
+const nume = document.getElementById('segn')
+var num1 = 0
+var num2 = 0
 
 function fazsoma(){
-    var soma = (prim+segn)
-    console.log('soma ', soma)
-    result.innerHTML = soma
+
+    num1 = Number(numer.value)
+    num2 = Number(nume.value)
+    var soma = (num1+num2)
+    console.log ('soma ', soma)
+    result.innerHTML = soma  
 }
+
+somar.addEventListener("click", fazsoma)
+
+function fazsubtracao(){
+
+    num1 = Number(numer.value)
+    num2 = Number(nume.value)
+    var subtr = (num1-num2)
+    console.log ('subtr ', subtr)
+    result.innerHTML = subtr  
+}
+
+somar.addEventListener("click", fazsubtracao)
+
+function fazsoma(){
+
+    num1 = Number(numer.value)
+    num2 = Number(nume.value)
+    var soma = (num1+num2)
+    console.log ('soma ', soma)
+    result.innerHTML = soma  
+}
+
+somar.addEventListener("click", fazsoma)
+
+function fazsoma(){
+
+    num1 = Number(numer.value)
+    num2 = Number(nume.value)
+    var soma = (num1+num2)
+    console.log ('soma ', soma)
+    result.innerHTML = soma  
+}
+
+somar.addEventListener("click", fazsoma)
+
+function fazsoma(){
+
+    num1 = Number(numer.value)
+    num2 = Number(nume.value)
+    var soma = (num1+num2)
+    console.log ('soma ', soma)
+    result.innerHTML = soma  
+}
+
+somar.addEventListener("click", fazsoma)
 
 
