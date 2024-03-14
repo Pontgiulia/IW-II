8d33bfe4115d91adb86967fafd131234a0f71d84
Pontgiulia <pontelligiulia@gmail.com>
2024-03-14 10:39:27 -0300

    imc

--- Aula04/js/script.js
+++ Aula04/js/script.js
@@ -1,9 +1,66 @@
-const altura = document.getElementById('altu')
-const peso = document.getElementById('pes')
-const masculino = document.getElementById('masc')
-const feminino = document.getElementById('fem')
-const result = document.getElementById('res')
+const img = document.getElementById('img')
 
-function imc(){
-    var bmi = (peso / (altura * altura)).toFixed(2);
+function calcular(){
+    var peso = parseFloat(document.getElementById('peso').value)
+    var altura = parseFloat(document.getElementById('altura').value)
+    var calcular = peso / (altura * altura)
+    resultIMC.innerHTML = calcular
+    diag(calcular)
+}
+
+function diag(calcular){
+    var sexo = document.getElementById('sexo').value
+    if(sexo == 'masc'){
+        if (calcular<20){
+            diagValor.innerHTML = 'Abaixo do peso'
+            // img.src = 'img/IMC6.png'
+        }
+        else if(calcular >= 20 && calcular<25){
+            diagValor.innerHTML = 'Normal'
+        }
+        else if(calcular>=25 && calcular<30){
+            diagValor.innerHTML = 'Obesidade leve'
+        }
+        else if(calcular>=30 && calcular<35){
+            diagValor.innerHTML = 'Obesidade grau I'
+        }
+        else if(calcular>=40){
+            diagValor.innerHTML = 'Obesidade grau III'
+        }
+        else{
+            diagValor.innerHTML = 'Obesidade grau II'
+        }
+    }
+    else if(sexo == 'fem'){
+        if (calcular<19){
+            diagValor.innerHTML = 'Abaixo do peso'
+            img = img.setAttribute('src','imc1.png')
+        }
+        else if(calcular >= 19 && calcular<24){
+            diagValor.innerHTML = 'Normal'
+        }
+        else if(calcular>=24 && calcular<29){
+            diagValor.innerHTML = 'Obesidade leve'
+        }
+        else if(calcular>=29 && calcular<34){
+            diagValor.innerHTML = 'Obesidade grau I'
+        }
+        else if(calcular>=39){
+            diagValor.innerHTML = 'Obesidade grau III'
+        }
+        else{
+            diagValor.innerHTML = 'Obesidade grau II'
+        }
+    }
+    else{
+        diagValor.innerHTML = 'Erro de dados'
+    }
+}
+
+function limpa(){
+    peso.value = ""
+    altura.value = ""
+    resultIMC.innerHTML = ""
+    diagValor.innerHTML = ""
+    genero.value = ""
 }
