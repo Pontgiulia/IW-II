988f04f5dfabd62b3e89d5a89d9e9b7de426b37e
Pontgiulia <pontelligiulia@gmail.com>
2024-06-06 11:31:11 -0300

    (:

--- curso_node/04_Html/teste.js
+++ curso_node/04_Html/teste.js
@@ -4,8 +4,40 @@ const sequileze = new Sequileze('test','root','',{
     dialect:'mysql'
 })
 
-sequileze.authenticate().then(function(){
-    console.log('Tudo funcionando (:')
-}).catch(function(erro){
-    console.log('Não tá funcionando porque: ' + erro)
+// sequileze.authenticate().then(function(){
+//     console.log('Tudo funcionando (:')
+// }).catch(function(erro){
+//     console.log('Não tá funcionando porque: ' + erro)
+// })
+
+const Postagem = sequileze.define('postagens',{
+    titulo:{
+        type:Sequileze.STRING
+    },
+    conteudo:{
+        type:Sequileze.TEXT
+    }
 })
+
+const Usuario = sequileze.define('usuarios',{
+    nome:{
+         type:Sequileze.STRING
+    },
+    sobrenome:{
+        type:Sequileze.STRING
+    },
+    email:{
+    type:Sequileze.STRING
+    },
+    Idade:{
+        type:Sequileze.INTEGER
+   }
+})
+
+// Postagem.sync(force=true)
+// Usuario.sync(force=true)
+
+// Postagem.create({
+//     titulo: 'Winxs',
+//     conteudo: 'Sei que você vai querer ser uma de nós! Winx, Quando damos nossas mãos, Nos tornamos poderosas, Porque juntas somos invencíveis'
+// })
