71ded6efed201e5d1bdefeafc6787f7b91f782e2
Pontgiulia <pontelligiulia@gmail.com>
2024-06-13 10:47:36 -0300

    blablabla

--- curso_node/05_Handlebars/index.js
+++ curso_node/05_Handlebars/index.js
@@ -1,5 +1,24 @@
 const express = require ("express")
 const app = express()
+const handlebars = require('express-handlebars')
+const Sequileze = require('sequelize')
+//config
+    //Template engine
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+
+
+    //conexão bd
+    const sequileze = new Sequileze('test','root','',{
+    host:'localhost',
+    dialect:'mysql'
+    })
+
+//rotas
+app.get('/cadastro', function(req,res){
+    res.render('Formulario')
+})
+
 
 app.listen(8081, function(){
     console.log('Servidor rodando em http://localhost:8081')
